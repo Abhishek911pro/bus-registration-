ea845a14959b17898299a6304fc766570457ba35
Abhishek_kumar <Abhishku611@gmail.com>
2024-08-23 11:27:44 +0530

    added entity package

---
+++ src/main/java/com/busproject/bus_registration/busEntity/Entity.java
@@ -0,0 +1,10 @@
+package com.busproject.bus_registration.busEntity;
+
+
+public class Entity {
+
+    long id ;
+    String userName;
+    String userEmail;
+    String userMobileNo;
+}
---
+++ src/test/java/com/busproject/bus_registration/BusRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busproject.bus_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
